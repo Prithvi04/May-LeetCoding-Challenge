a6b9e01a7d292e56f609650ead3e7eca8aea5c37
Prithviraj N Indi <38948106+Prithvi04@users.noreply.github.com>
2020-05-02 13:08:30 +0530

    Add files via upload

---
+++ Week-1/Day_2_JewelsandStones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    //use a hashmap to count the number of occurances of each stone and count the number of jewels.
+    int numJewelsInStones(string J, string S) {
+        unordered_map<char,int>mp;
+        for(char ch:S)
+            mp[ch]++;
+        
+        int count=0;
+        for(char ch:J)
+        {
+            if(mp.find(ch)!=mp.end())
+                count+=mp[ch];
+        }
+        return count;
+    }
+};
