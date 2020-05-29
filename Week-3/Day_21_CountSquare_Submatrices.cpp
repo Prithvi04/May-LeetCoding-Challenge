59812babe1cb34e1e67c1874f8ef819e973a5af3
Prithviraj N Indi <38948106+Prithvi04@users.noreply.github.com>
2020-05-29 20:13:43 +0530

    Add files via upload

---
+++ Week-3/Day_21_CountSquare_Submatrices.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        if(matrix.size()==0)
+            return 0;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int count=0;
+        
+        int dp[m][n];
+        for(int i=0;i<m;i++)
+        {
+            dp[i][0]=matrix[i][0];
+            count+=matrix[i][0];
+        }
+        for(int j=1;j<n;j++)
+        {
+            dp[0][j]=matrix[0][j];
+            count+=matrix[0][j];
+            
+        }
+        
+        for(int i=1;i<m;i++)
+        {
+            
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j]=0;
+                if(matrix[i][j]==1)
+                dp[i][j]=1+min(min(dp[i-1][j-1],dp[i-1][j]),dp[i][j-1]);
+                count+=dp[i][j];
+            }
+        }
+        return count;
+            
+        
+    }
+};
+//Popular DP problem
+//Filling table based on Top down approach.
